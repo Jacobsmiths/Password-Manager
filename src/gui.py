679c5d02e621b8bd828f64560c1ef5ee64b0d779
Jacob Smith <jaco.smith589@gmail.com>
2024-06-14 23:20:45 -0500

    removed user class and can switch frames

--- src/gui.py
+++ src/gui.py
@@ -1,32 +1,46 @@
 from tkinter import messagebox
 import customtkinter as ctk
-import service
 
 ctk.set_appearance_mode("dark")
 
 class app(ctk.CTk):
-    def __init__(self, userService, authService, user):
+    def __init__(self, userService, authService):
         super().__init__()
         self.geometry("1000x600")
         self.title("Jacob's Ultra Protected Password Manager")
 
         self.userService = userService
         self.authService = authService
-        self.user = user
 
-        self.current_frame = initialLoginFrame(self,self.userService, self.authService, self.user)
+        container = ctk.CTkFrame(self)
+        container.pack(fill="both", expand=True)
+        # container.grid_rowconfigure(0, weight=1)
+        # container.grid_columnconfigure(0, weight=1)
 
-        self.mainloop()
+        self.frames = {
+            "initialLoginFrame" : initialLoginFrame(parent=container, controller=self, userService=self.userService, authService=self.authService),
+            "passwordDisplayFrame": passwordDisplayFrame(parent=container, userService=self.userService)
+        }
+    
+
+        self.current_frame = self.frames[initialLoginFrame.__name__]
+        self.current_frame.pack(expand=True, fill="both")
+
+    def showFrame(self, frame):
+        self.current_frame.pack_forget()
+        self.current_frame = self.frames[frame.__name__]
+        self.current_frame.pack(expand=True, fill="both")
+       
         
 
 class initialLoginFrame(ctk.CTkFrame):
-    def __init__(self, parent, userService, authService, user, **kwargs):
+    def __init__(self, parent, controller, userService, authService, **kwargs):
         super().__init__(parent, **kwargs)
-        self.pack(expand=True, fill="both")
+        # self.pack(expand=True, fill="both")
 
         self.userService = userService
         self.authService = authService
-        self.user = user
+        self.controller = controller
 
         self.label = ctk.CTkLabel(self, text='Login',corner_radius=8, font=("Helvetica", 20))
         self.label.pack(pady=20)
@@ -44,24 +58,31 @@ class initialLoginFrame(ctk.CTkFrame):
     def retrieveUser(self):
         """Checks if user exists, if they don't then ask if they want to make new user. If the user does exists
             It compares passwords to gain access to account"""
-        
-        if(self.user.password):
-            self.user.setUsername(self.usr_entry.get())
-            self.user.setPassword(self.authService(self.pass_entry.get()))
+        username = self.usr_entry.get()
+        password = self.pass_entry.get()
 
         # checks if user exists using the userService class
-        if(self.userService.checkUser(self.user.username)):
-            self.authService.verifyUser(self.user.passowrd, self.userService.user_data_file_local)
+        if(self.userService.checkUser(username)):
+            if(self.authService.verifyUser(username, password)):
+                print("correct Password")
+                self.controller.showFrame(passwordDisplayFrame)
+                
+            else: 
+                print("incorrect password")
         else:
             response = messagebox.askyesno("No User Found", "Do you want to create a new user?")
             if response:
-                self.userService.setUpUser(usr, passwrd, self.authService)
+                self.userService.setUpUser(username, password, self.authService)
 
 
-class newUserFrame(ctk.CTkFrame):
+class passwordDisplayFrame(ctk.CTkFrame):
     def __init__(self, parent, userService, **kwargs):
         super().__init__(parent, **kwargs)
+        # self.pack(expand=True, fill="both")
+
         self.userService = userService
-        self.pack(expand=True, fill="Both")
+
+        self.lab = ctk.CTkLabel(self, text='FART!!!!!',corner_radius=8, font=("Helvetica", 20))
+        self.lab.pack()
 
         
--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ import os
 from gui import app
 import service
 
-user = service.User()
-user_service = service.UserService(user)
-auth_service = service.AuthService(user)
-app = app(user_service, auth_service, user)
+
+user_service = service.UserService()
+auth_service = service.AuthService()
+app = app(user_service, auth_service)
+
+app.mainloop()
